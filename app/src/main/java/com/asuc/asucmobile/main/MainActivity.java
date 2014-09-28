e6a0f9b6175075561adba6c9a5d97412f4df6be9
Alex Kang <akang95@gmail.com>
2014-09-27 23:59:46 -0700

    Fixed some UI elements and improved search to include locations

--- app/src/main/java/com/asuc/asucmobile/main/MainActivity.java
+++ app/src/main/java/com/asuc/asucmobile/main/MainActivity.java
@@ -13,6 +13,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent i = new Intent(this, LibraryActivity.class);
+        startActivity(i);
     }
 
 }
