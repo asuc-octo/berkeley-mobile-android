c5beea2043bfe49b36f3c3a1a6cee8e36594955c
Alex Kang <akang95@gmail.com>
2014-09-28 22:26:49 -0700

    Implemented gyms

--- app/src/main/java/com/asuc/asucmobile/main/GymActivity.java
+++ app/src/main/java/com/asuc/asucmobile/main/GymActivity.java
@@ -3,57 +3,53 @@ package com.asuc.asucmobile.main;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Filter;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ProgressBar;
-import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.asuc.asucmobile.R;
-import com.asuc.asucmobile.adapters.LibraryAdapter;
-import com.asuc.asucmobile.controllers.LibraryController;
-import com.asuc.asucmobile.models.Library;
+import com.asuc.asucmobile.adapters.GymAdapter;
+import com.asuc.asucmobile.controllers.GymController;
+import com.asuc.asucmobile.models.Gym;
 import com.asuc.asucmobile.utilities.Callback;
 
 import java.util.ArrayList;
 
-public class LibraryActivity extends Activity {
+public class GymActivity extends Activity {
 
-    private ListView mLibraryList;
+    private ListView mGymList;
     private ProgressBar mProgressBar;
     private LinearLayout mRefreshWrapper;
 
-    private LibraryAdapter mAdapter;
+    private GymAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_library);
+        setContentView(R.layout.activity_gym);
         if (getActionBar() != null) {
             getActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
         ImageButton refreshButton = (ImageButton) findViewById(R.id.refresh_button);
 
-        mLibraryList = (ListView) findViewById(R.id.library_list);
+        mGymList = (ListView) findViewById(R.id.gym_list);
         mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
         mRefreshWrapper = (LinearLayout) findViewById(R.id.refresh);
 
-        mAdapter = new LibraryAdapter(this);
-        mLibraryList.setAdapter(mAdapter);
+        mAdapter = new GymAdapter(this);
+        mGymList.setAdapter(mAdapter);
 
-        mLibraryList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mGymList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                OpenLibraryActivity.staticLibrary = mAdapter.getItem(i);
-                Intent intent = new Intent(getBaseContext(), OpenLibraryActivity.class);
+                OpenGymActivity.staticGym = mAdapter.getItem(i);
+                Intent intent = new Intent(getBaseContext(), OpenGymActivity.class);
                 startActivity(intent);
             }
         });
@@ -74,31 +70,6 @@ public class LibraryActivity extends Activity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.library, menu);
-
-        final SearchView search = (SearchView) menu.findItem(R.id.search).getActionView();
-        search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String s) {
-                // Close the keyboard
-                search.clearFocus();
-                return true;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String s) {
-                final Filter filter = mAdapter.getFilter();
-                filter.filter(s);
-                return true;
-            }
-        });
-
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -116,18 +87,18 @@ public class LibraryActivity extends Activity {
      * from the web.
      */
     private void refresh() {
-        mLibraryList.setVisibility(View.GONE);
+        mGymList.setVisibility(View.GONE);
         mRefreshWrapper.setVisibility(View.GONE);
         mProgressBar.setVisibility(View.VISIBLE);
 
-        LibraryController.getInstance(this).refreshInBackground(new Callback() {
+        GymController.getInstance(this).refreshInBackground(new Callback() {
             @Override
             @SuppressWarnings("unchecked")
             public void onDataRetrieved(Object data) {
-                mLibraryList.setVisibility(View.VISIBLE);
+                mGymList.setVisibility(View.VISIBLE);
                 mProgressBar.setVisibility(View.GONE);
 
-                mAdapter.setList((ArrayList<Library>) data);
+                mAdapter.setList((ArrayList<Gym>) data);
             }
 
             @Override
