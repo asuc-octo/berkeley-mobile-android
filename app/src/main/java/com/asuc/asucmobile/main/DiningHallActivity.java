68653fa70796474385ed9d3ccb59dacf0fe1c91a
Alex Kang <akang95@gmail.com>
2014-09-30 21:04:50 -0700

    Added everything except dining halls

---
+++ app/src/main/java/com/asuc/asucmobile/main/DiningHallActivity.java
@@ -0,0 +1,16 @@
+package com.asuc.asucmobile.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.asuc.asucmobile.R;
+
+public class DiningHallActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dining_hall);
+    }
+
+}
