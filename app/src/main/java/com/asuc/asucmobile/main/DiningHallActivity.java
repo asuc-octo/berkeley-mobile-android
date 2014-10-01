d326676d08076f8887475969cd3d1bc9b2176b71
Alex Kang <akang95@gmail.com>
2014-09-30 23:14:07 -0700

    Added dining hall list

--- app/src/main/java/com/asuc/asucmobile/main/DiningHallActivity.java
+++ app/src/main/java/com/asuc/asucmobile/main/DiningHallActivity.java
@@ -3,11 +3,30 @@ package com.asuc.asucmobile.main;
 import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.asuc.asucmobile.R;
+import com.asuc.asucmobile.adapters.DiningHallAdapter;
+import com.asuc.asucmobile.controllers.DiningController;
+import com.asuc.asucmobile.models.DiningHall;
+import com.asuc.asucmobile.utilities.Callback;
+
+import java.util.ArrayList;
 
 public class DiningHallActivity extends Activity {
+
+    private ListView mDiningList;
+    private ProgressBar mProgressBar;
+    private LinearLayout mRefreshWrapper;
+
+    private DiningHallAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +37,63 @@ public class DiningHallActivity extends Activity {
             titleText.setTypeface(Typeface.createFromAsset(getAssets(), "young.ttf"));
         }
         setContentView(R.layout.activity_dining_hall);
+
+        ImageButton refreshButton = (ImageButton) findViewById(R.id.refresh_button);
+
+        mDiningList = (ListView) findViewById(R.id.dining_hall_list);
+        mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        mRefreshWrapper = (LinearLayout) findViewById(R.id.refresh);
+
+        mAdapter = new DiningHallAdapter(this);
+        mDiningList.setAdapter(mAdapter);
+
+        mDiningList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            }
+        });
+
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                refresh();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        refresh();
+    }
+
+    /**
+     * refresh() updates the visibility of necessary UI elements and refreshes the dining hall list
+     * from the web.
+     */
+    private void refresh() {
+        mDiningList.setVisibility(View.GONE);
+        mRefreshWrapper.setVisibility(View.GONE);
+        mProgressBar.setVisibility(View.VISIBLE);
+
+        DiningController.getInstance(this).refreshInBackground(new Callback() {
+            @Override
+            @SuppressWarnings("unchecked")
+            public void onDataRetrieved(Object data) {
+                mDiningList.setVisibility(View.VISIBLE);
+                mProgressBar.setVisibility(View.GONE);
+
+                mAdapter.setList((ArrayList<DiningHall>) data);
+            }
+
+            @Override
+            public void onRetrievalFailed() {
+                mProgressBar.setVisibility(View.GONE);
+                mRefreshWrapper.setVisibility(View.VISIBLE);
+                Toast.makeText(getBaseContext(), "Unable to retrieve data, please try again", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
