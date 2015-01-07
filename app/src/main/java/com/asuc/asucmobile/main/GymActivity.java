963ef174709d4e6424b22997b411e0df42068ab3
Anders Truong <anderstruong@IglooAustralia.local>
2015-01-07 02:01:33 -0800

    Cleaning
    Cleaning

--- app/src/main/java/com/asuc/asucmobile/main/GymActivity.java
+++ app/src/main/java/com/asuc/asucmobile/main/GymActivity.java
@@ -36,6 +36,7 @@ public class GymActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         FlurryAgent.onStartSession(this, "4VPTT49FCCKH7Z2NVQ26");
 
         if (getActionBar() != null) {
@@ -45,6 +46,7 @@ public class GymActivity extends Activity {
 
             getActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
         setContentView(R.layout.activity_gym);
 
         ImageButton refreshButton = (ImageButton) findViewById(R.id.refresh_button);
@@ -57,50 +59,55 @@ public class GymActivity extends Activity {
         mGymList.setAdapter(mAdapter);
 
         mGymList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                GymController controller = ((GymController) GymController.getInstance(getBaseContext()));
+                GymController controller = ((GymController)
+                        GymController.getInstance(getBaseContext()));
                 controller.setCurrentGym(mAdapter.getItem(i));
                 Intent intent = new Intent(getBaseContext(), OpenGymActivity.class);
 
-                //Flurry log for tapping Gyms.
+                // Flurry log for tapping "Gyms".
                 Map<String, String> gymParams = new HashMap<String, String>();
                 gymParams.put("Hall", mAdapter.getItem(i).getName());
                 FlurryAgent.logEvent("Taps Gym Hours", gymParams);
 
                 startActivity(intent);
             }
+
         });
 
         refreshButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
                 refresh();
             }
+
         });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
         refresh();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-
         FlurryAgent.onEndSession(this);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
+
         if (id == android.R.id.home) {
             finish();
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
@@ -108,26 +115,24 @@ public class GymActivity extends Activity {
     public void onBackPressed() {
         super.onBackPressed();
 
-        //Flurry logging for pressing the Back Button
+        // Flurry log for pressing the back button
         FlurryAgent.logEvent("Tapped on the Back Button (Gyms)");
     }
 
-    /**
-     * refresh() updates the visibility of necessary UI elements and refreshes the library list
-     * from the web.
-     */
+    /** Updates the visibility of necessary UI elements; refreshes the dining hall list
+     *  from the web. */
     private void refresh() {
         mGymList.setVisibility(View.GONE);
         mRefreshWrapper.setVisibility(View.GONE);
         mProgressBar.setVisibility(View.VISIBLE);
 
         GymController.getInstance(this).refreshInBackground(new Callback() {
+
             @Override
             @SuppressWarnings("unchecked")
             public void onDataRetrieved(Object data) {
                 mGymList.setVisibility(View.VISIBLE);
                 mProgressBar.setVisibility(View.GONE);
-
                 mAdapter.setList((ArrayList<Gym>) data);
             }
 
@@ -135,8 +140,10 @@ public class GymActivity extends Activity {
             public void onRetrievalFailed() {
                 mProgressBar.setVisibility(View.GONE);
                 mRefreshWrapper.setVisibility(View.VISIBLE);
-                Toast.makeText(getBaseContext(), "Unable to retrieve data, please try again", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getBaseContext(),
+                        "Unable to retrieve data", Toast.LENGTH_SHORT).show();
             }
+
         });
     }
 
