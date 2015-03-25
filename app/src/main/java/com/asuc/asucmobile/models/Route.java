78f6b02dd0872b8fc1b4a9f122ea51499cb768dc
Alex Kang <akang95@gmail.com>
2015-03-24 22:15:23 -0700

    Handled one stop trip edge case

--- app/src/main/java/com/asuc/asucmobile/models/Route.java
+++ app/src/main/java/com/asuc/asucmobile/models/Route.java
@@ -27,7 +27,7 @@ public class Route {
     }
 
     public ArrayList<Stop> getStopsAndTransfers() {
-        ArrayList<Stop> stops = new ArrayList<Stop>();
+        ArrayList<Stop> stops = new ArrayList<>();
 
         Date previousArrival = null;
         for (Trip trip : trips) {
@@ -37,6 +37,10 @@ public class Route {
             for (Stop stop : trip.getStops()) {
                 stops.add(stop);
             }
+        }
+
+        if (stops.size() == 2) { // One trip and one stop list. Need to add extra stop.
+            stops.add(trips.get(0).getStops().get(0));
         }
 
         stops.add(new Stop("", null, previousArrival));
