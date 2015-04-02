d4e121ce3d8c12a450b06539c9db8a2cab045885
Alex Kang <akang95@gmail.com>
2015-04-01 18:32:54 -0700

    Added in Ankits Google Play location retrieval

--- app/src/main/java/com/asuc/asucmobile/main/StopActivity.java
+++ app/src/main/java/com/asuc/asucmobile/main/StopActivity.java
@@ -1,9 +1,6 @@
 package com.asuc.asucmobile.main;
 
 import android.app.Activity;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.graphics.Typeface;
@@ -35,10 +32,9 @@ import com.asuc.asucmobile.models.Stop;
 import com.asuc.asucmobile.utilities.Callback;
 import com.flurry.android.FlurryAgent;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.location.LocationServices;
 
 import java.util.ArrayList;
@@ -69,8 +65,6 @@ public class StopActivity extends Activity implements ConnectionCallbacks, OnCon
 
     // Request code to use when launching the resolution activity
     private static final int REQUEST_RESOLVE_ERROR = 1001;
-    // Unique tag for the error dialog fragment
-    private static final String DIALOG_ERROR = "dialog_error";
     // Bool to track whether the app is already resolving an error
     private boolean mResolvingError = false;
 
@@ -97,8 +91,8 @@ public class StopActivity extends Activity implements ConnectionCallbacks, OnCon
         // Get location manager
         final LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         String bestProvider = locationManager.getBestProvider(new Criteria(), false);
-//
-//        // For quick access, retrieve the last location update
+
+        // For quick access, retrieve the last location update
         Location location = locationManager.getLastKnownLocation(bestProvider);
         if (location != null) {
             mLatitude = location.getLatitude();
@@ -180,10 +174,7 @@ public class StopActivity extends Activity implements ConnectionCallbacks, OnCon
 
     @Override
     public void onConnectionFailed(ConnectionResult result) {
-        if (mResolvingError) {
-            // Already attempting to resolve an error.
-            return;
-        } else if (result.hasResolution()) {
+        if (result.hasResolution()) {
             try {
                 mResolvingError = true;
                 result.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
@@ -191,10 +182,6 @@ public class StopActivity extends Activity implements ConnectionCallbacks, OnCon
                 // There was an error with the resolution intent. Try again.
                 mGoogleApiClient.connect();
             }
-        } else {
-            // Show dialog using GooglePlayServicesUtil.getErrorDialog()
-            showErrorDialog(result.getErrorCode());
-            mResolvingError = true;
         }
     }
 
@@ -214,42 +201,6 @@ public class StopActivity extends Activity implements ConnectionCallbacks, OnCon
                     mGoogleApiClient.connect();
                 }
             }
-        }
-    }
-
-    // The rest of this code is all about building the error dialog
-
-    /* Creates a dialog for an error message */
-    private void showErrorDialog(int errorCode) {
-        // Create a fragment for the error dialog
-        ErrorDialogFragment dialogFragment = new ErrorDialogFragment();
-        // Pass the error that should be displayed
-        Bundle args = new Bundle();
-        args.putInt(DIALOG_ERROR, errorCode);
-        dialogFragment.setArguments(args);
-//        dialogFragment.show(getSupportFragmentManager(), "errordialog");
-    }
-
-    /* Called from ErrorDialogFragment when the dialog is dismissed. */
-    public void onDialogDismissed() {
-        mResolvingError = false;
-    }
-
-    /* A fragment to display an error dialog */
-    public static class ErrorDialogFragment extends DialogFragment {
-        public ErrorDialogFragment() { }
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Get the error code and retrieve the appropriate dialog
-            int errorCode = this.getArguments().getInt(DIALOG_ERROR);
-            return GooglePlayServicesUtil.getErrorDialog(errorCode,
-                    this.getActivity(), REQUEST_RESOLVE_ERROR);
-        }
-
-        @Override
-        public void onDismiss(DialogInterface dialog) {
-//            ((MainActivity)getActivity()).onDialogDismissed();
         }
     }
 
