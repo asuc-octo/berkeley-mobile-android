ab78068e1a94a77f60a8308131f7aa16adda23fe
Ankit Patel <ankitpatel96@gmail.com>
2015-09-30 21:51:33 -0700

    Wrote logic to gather data from ListStartStopActivity upon activity result

--- app/src/main/java/com/asuc/asucmobile/main/StartStopSelectActivity.java
+++ app/src/main/java/com/asuc/asucmobile/main/StartStopSelectActivity.java
@@ -2,16 +2,22 @@ package com.asuc.asucmobile.main;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageButton;
 
 import com.asuc.asucmobile.R;
 import com.asuc.asucmobile.adapters.StopAdapter;
+import com.asuc.asucmobile.controllers.LineController;
+import com.asuc.asucmobile.models.Stop;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 
+//TODO: flurry
 public class StartStopSelectActivity extends AppCompatActivity
     implements OnMapReadyCallback {
 
@@ -19,7 +25,11 @@ public class StartStopSelectActivity extends AppCompatActivity
     private static final int END_INT = 2;
 
     private MapFragment mapFragment;
-    private StopAdapter mAdapter;
+    private ImageButton startButton;
+    private ImageButton endButton;
+
+    private Stop startStop;
+    private Stop endStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +38,59 @@ public class StartStopSelectActivity extends AppCompatActivity
 
         mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        View startButton = findViewById(R.id.select_start);
-        View endButton =  findViewById(R.id.select_dest);
+        startButton = (ImageButton) findViewById(R.id.select_start);
+        endButton =  (ImageButton) findViewById(R.id.select_dest);
         final Context context = getApplicationContext();
         startButton.setOnClickListener(new StartStopListener(START_INT, context));
         endButton.setOnClickListener(new StartStopListener(END_INT, context));
+        FloatingActionButton navigateButton = (FloatingActionButton) findViewById(R.id.navigate_button);
+        navigateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), OpenRouteSelectionActivity.class);
+
+                intent.putExtra("stop_id", mAdapter.getItem(i).getId());
+                intent.putExtra("stop_name", mAdapter.getItem(i).getName());
+                intent.putExtra("lat", mLatitude);
+                intent.putExtra("long", mLongitude);
+
+                startActivity(intent);
+            }
+        });3
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_CANCELED) {
+            return;
+        }
+        SharedPreferences pref = getSharedPreferences("startStopSelect", MODE_PRIVATE);
+        if (requestCode == START_INT) {
+            getStartFromPref();
+            startButton.setText(startStop.getName());
+        } else if (requestCode == END_INT) {
+            getEndFromPref();
+            endButton.setText(endStop.getName());
+        }
+//        int[] latLong = data.getIntArrayExtra("latLong");
+//        String locName = data.getStringExtra("name");
+
+    }
+
+    private void getStartFromPref() {
+        SharedPreferences pref = getSharedPreferences("startStopSelect", MODE_PRIVATE);
+        Integer startId = pref.getInt("start_id", -1);
+        String startName = pref.getString("start_name", "");
+        //TODO: error checking
+        startStop = ((LineController) (LineController.getInstance(this))).getStop(startId, startName);
+    }
+
+    private void getEndFromPref() {
+        SharedPreferences pref = getSharedPreferences("startStopSelect", MODE_PRIVATE);
+        Integer endId = pref.getInt("end_id", -1);
+        String endName = pref.getString("end_name", "");
+        //TODO: error checking
+        endStop = ((LineController) (LineController.getInstance(this))).getStop(endId, endName);
     }
 
     private class StartStopListener implements View.OnClickListener {
