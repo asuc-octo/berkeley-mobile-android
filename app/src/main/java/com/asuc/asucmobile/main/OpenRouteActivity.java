7c12caa3030d9f8b6a556a998b342a81ff964a35
Ankit Patel <ankitpatel96@gmail.com>
2015-10-17 17:00:43 -0700

    fix for menu item background, involved changing styles, manually fixed list item bottom border and action bar title text color

--- app/src/main/java/com/asuc/asucmobile/main/OpenRouteActivity.java
+++ app/src/main/java/com/asuc/asucmobile/main/OpenRouteActivity.java
@@ -53,6 +53,7 @@ public class OpenRouteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_open_route);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.off_white));
         toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_action_back));
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/asuc/asucmobile/main/OpenRouteSelectionActivity.java
+++ app/src/main/java/com/asuc/asucmobile/main/OpenRouteSelectionActivity.java
@@ -42,6 +42,7 @@ public class OpenRouteSelectionActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.off_white));
         toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_action_back));
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
