3a4046e4ad2e1420d0063bac0272ebe58f32b623
Ankit Patel <ankitpatel96@gmail.com>
2015-12-31 13:11:33 -0800

    added line controller network call to OpenRouteSelectionActivity, chained callback for RouteController

--- app/src/main/java/com/asuc/asucmobile/main/OpenRouteSelectionActivity.java
+++ app/src/main/java/com/asuc/asucmobile/main/OpenRouteSelectionActivity.java
@@ -17,7 +17,6 @@ import com.asuc.asucmobile.adapters.RouteSelectionAdapter;
 import com.asuc.asucmobile.controllers.LineController;
 import com.asuc.asucmobile.controllers.RouteController;
 import com.asuc.asucmobile.models.Route;
-import com.asuc.asucmobile.models.Stop;
 import com.asuc.asucmobile.utilities.Callback;
 import com.flurry.android.FlurryAgent;
 import com.google.android.gms.maps.model.LatLng;
@@ -30,7 +29,8 @@ public class OpenRouteSelectionActivity extends AppCompatActivity {
     private ProgressBar mProgressBar;
     private LinearLayout mRefreshWrapper;
 
-    private RouteController mController;
+    private RouteController routeController;
+    private LineController lineController;
     private RouteSelectionAdapter mAdapter;
 
     @Override
@@ -50,19 +50,8 @@ public class OpenRouteSelectionActivity extends AppCompatActivity {
             }
         });
 
-        int stopId = getIntent().getIntExtra("stop_id", -1);
-        String stopName = getIntent().getStringExtra("stop_name");
-        double lat = getIntent().getDoubleExtra("lat", -1.0);
-        double lng = getIntent().getDoubleExtra("long", -1.0);
-        if (stopId == -1 || lat == -1.0 || lng == -1.0) {
-            finish();
-            return;
-        }
-
-        if (((LineController) (LineController.getInstance(this))).getStops().size() == 0) {
-            finish();
-            return;
-        }
+        LatLng startLatLng = getIntent().getParcelableExtra("startLngLat");
+        LatLng endLatLng = getIntent().getParcelableExtra("endLngLat");
 
         ImageButton mRefreshButton = (ImageButton) findViewById(R.id.refresh_button);
 
@@ -70,9 +59,8 @@ public class OpenRouteSelectionActivity extends AppCompatActivity {
         mRefreshWrapper = (LinearLayout) findViewById(R.id.refresh);
         mTripList = (ListView) findViewById(R.id.tripList);
 
-        Stop dest = ((LineController) (LineController.getInstance(this))).getStop(stopId, stopName);
-        mController = (RouteController) RouteController.createInstance(this, new LatLng(lat, lng), dest.getLocation());
-
+        routeController = (RouteController) RouteController.createInstance(this, startLatLng, endLatLng);
+        lineController = (LineController) LineController.getInstance(this);
         mRefreshButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -94,31 +82,42 @@ public class OpenRouteSelectionActivity extends AppCompatActivity {
         mTripList.setVisibility(View.GONE);
         mRefreshWrapper.setVisibility(View.GONE);
         mProgressBar.setVisibility(View.VISIBLE);
-
-        mController.refreshInBackground(new Callback() {
+        lineController.refreshInBackground(new Callback() {
             @Override
-            @SuppressWarnings("unchecked")
             public void onDataRetrieved(Object data) {
-                mProgressBar.setVisibility(View.INVISIBLE);
-                mTripList.setVisibility(View.VISIBLE);
+                routeController.refreshInBackground(new Callback() {
+                    @Override
+                    @SuppressWarnings("unchecked")
+                    public void onDataRetrieved(Object data) {
+                        mProgressBar.setVisibility(View.INVISIBLE);
+                        mTripList.setVisibility(View.VISIBLE);
 
-                ArrayList<Route> routes = (ArrayList<Route>) data;
+                        ArrayList<Route> routes = (ArrayList<Route>) data;
 
-                if (routes.size() > 0) {
-                    mAdapter = new RouteSelectionAdapter(getBaseContext(), routes);
-                    mTripList.setAdapter(mAdapter);
+                        if (routes.size() > 0) {
+                            mAdapter = new RouteSelectionAdapter(getBaseContext(), routes);
+                            mTripList.setAdapter(mAdapter);
 
-                    mTripList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                        @Override
-                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                            mController.setCurrentRoute(mAdapter.getItem(position));
-                            Intent intent = new Intent(getBaseContext(), OpenRouteActivity.class);
-                            startActivity(intent);
+                            mTripList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                    routeController.setCurrentRoute(mAdapter.getItem(position));
+                                    Intent intent = new Intent(getBaseContext(), OpenRouteActivity.class);
+                                    startActivity(intent);
+                                }
+                            });
+                        } else {
+                            mTripList.setVisibility(View.INVISIBLE);
                         }
-                    });
-                } else {
-                    mTripList.setVisibility(View.INVISIBLE);
-                }
+                    }
+
+                    @Override
+                    public void onRetrievalFailed() {
+                        mProgressBar.setVisibility(View.GONE);
+                        mRefreshWrapper.setVisibility(View.VISIBLE);
+                        Toast.makeText(getBaseContext(), "Unable to retrieve data, please try again", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
 
             @Override
@@ -128,6 +127,7 @@ public class OpenRouteSelectionActivity extends AppCompatActivity {
                 Toast.makeText(getBaseContext(), "Unable to retrieve data, please try again", Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 
 }
