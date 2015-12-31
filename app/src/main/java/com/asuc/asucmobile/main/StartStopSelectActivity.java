d4b3d4a2774a3de134e8d1fdef9dc180a9f68068
Ankit Patel <ankitpatel96@gmail.com>
2015-12-31 12:03:42 -0800

    linked together stopactivity and startstopselectactivity

--- app/src/main/java/com/asuc/asucmobile/main/StartStopSelectActivity.java
+++ app/src/main/java/com/asuc/asucmobile/main/StartStopSelectActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.asuc.asucmobile.R;
 import com.asuc.asucmobile.adapters.StopAdapter;
@@ -17,20 +18,27 @@ import com.asuc.asucmobile.models.Stop;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 
 //TODO: flurry
+//TODO: check for existing location through sharedPreferences
 public class StartStopSelectActivity extends AppCompatActivity
     implements OnMapReadyCallback {
 
     private static final int START_INT = 1;
     private static final int END_INT = 2;
 
+    private Context context;
+
     private MapFragment mapFragment;
     private Button startButton;
     private Button endButton;
 
-    private Stop startStop;
-    private Stop endStop;
+    private String startName;
+    private String endName;
+
+    private LatLng startLatLng;
+    private LatLng endLatLng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,23 +53,28 @@ public class StartStopSelectActivity extends AppCompatActivity
 
         navigateButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_navigation, null));
 
-        final Context context = getApplicationContext();
-        startButton.setOnClickListener(new StartStopListener(START_INT, context));
-        endButton.setOnClickListener(new StartStopListener(END_INT, context));
+        context = getBaseContext();
+        startButton.setOnClickListener(new StartStopListener(START_INT));
+        endButton.setOnClickListener(new StartStopListener(END_INT));
 
         navigateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getBaseContext(), OpenRouteSelectionActivity.class);
-
-                intent.putExtra("stop_id", mAdapter.getItem(i).getId());
-                intent.putExtra("stop_name", mAdapter.getItem(i).getName());
-                intent.putExtra("lat", mLatitude);
-                intent.putExtra("long", mLongitude);
+                if(startLatLng == null || endLatLng == null) {
+                    Toast.makeText(getBaseContext(), "Please select a start and an end location", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                intent.putExtra("startLngLat", startLatLng);
+                intent.putExtra("endLngLat", endLatLng);
+//                intent.putExtra("stop_id", mAdapter.getItem(i).getId());
+//                intent.putExtra("stop_name", mAdapter.getItem(i).getName());
+//                intent.putExtra("lat", mLatitude);
+//                intent.putExtra("long", mLongitude);
 
                 startActivity(intent);
             }
-        });3
+        });
     }
 
     @Override
@@ -69,46 +82,46 @@ public class StartStopSelectActivity extends AppCompatActivity
         if (resultCode == RESULT_CANCELED) {
             return;
         }
-        SharedPreferences pref = getSharedPreferences("startStopSelect", MODE_PRIVATE);
         if (requestCode == START_INT) {
-            getStartFromPref();
-            startButton.setText(startStop.getName());
+            getStartFromPref(data);
+//            startButton.setText(startStop.getName());
+            startButton.setText(startName);
         } else if (requestCode == END_INT) {
-            getEndFromPref();
-            endButton.setText(endStop.getName());
+            getEndFromPref(data);
+//            endButton.setText(endStop.getName());
+            endButton.setText(endName);
         }
 //        int[] latLong = data.getIntArrayExtra("latLong");
 //        String locName = data.getStringExtra("name");
 
     }
 
-    private void getStartFromPref() {
-        SharedPreferences pref = getSharedPreferences("startStopSelect", MODE_PRIVATE);
-        Integer startId = pref.getInt("start_id", -1);
-        String startName = pref.getString("start_name", "");
+    private void getStartFromPref(Intent data) {
+        startName = data.getStringExtra("startName");
+        startLatLng = data.getParcelableExtra("startLatLng");
         //TODO: error checking
-        startStop = ((LineController) (LineController.getInstance(this))).getStop(startId, startName);
+//        startStop = ((LineController) (LineController.getInstance(this))).getStop(startId, startName);
     }
 
-    private void getEndFromPref() {
-        SharedPreferences pref = getSharedPreferences("startStopSelect", MODE_PRIVATE);
-        Integer endId = pref.getInt("end_id", -1);
-        String endName = pref.getString("end_name", "");
+    private void getEndFromPref(Intent data) {
+        endName = data.getStringExtra("endName");
+        endLatLng = data.getParcelableExtra("endLatLng");
         //TODO: error checking
-        endStop = ((LineController) (LineController.getInstance(this))).getStop(endId, endName);
+//        endStop = ((LineController) (LineController.getInstance(this))).getStop(endId, endName);
     }
 
     private class StartStopListener implements View.OnClickListener {
 
         private int typeRequest;
-        private Context context;
 
-        public StartStopListener(int typeRequest, Context context) {
+        public StartStopListener(int typeRequest) {
             this.typeRequest = typeRequest;
         }
         @Override
         public void onClick(View view) {
-            startActivityForResult(new Intent(context, ListStartStopActivity.class), typeRequest);
+            Intent intent = new Intent(context, StopActivity.class);
+            intent.putExtra("requestCode", typeRequest);
+            startActivityForResult(intent, typeRequest);
         }
     }
 
