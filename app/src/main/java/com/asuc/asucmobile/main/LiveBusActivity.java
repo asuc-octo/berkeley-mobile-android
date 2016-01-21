fa0028591c1edef38af81b93e42d24068497d085
Alex Kang <akang95@gmail.com>
2016-01-20 18:47:05 -0800

    FIXED BEARTRANSIT UI BUGS MOFO

--- app/src/main/java/com/asuc/asucmobile/main/LiveBusActivity.java
+++ app/src/main/java/com/asuc/asucmobile/main/LiveBusActivity.java
@@ -1,8 +1,13 @@
 package com.asuc.asucmobile.main;
 
+import android.Manifest;
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
@@ -17,6 +22,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -26,7 +33,6 @@ import java.util.TimerTask;
 
 public class LiveBusActivity extends AppCompatActivity implements OnMapReadyCallback{
 
-    private GoogleMap map;
     private Timer timer;
     private LiveBusActivity activity;
     private MapFragment mapFragment;
@@ -59,12 +65,14 @@ public class LiveBusActivity extends AppCompatActivity implements OnMapReadyCall
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        map = googleMap;
-        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-        map.setMyLocationEnabled(true);
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            googleMap.setMyLocationEnabled(true);
+        }
         CameraUpdate update = CameraUpdateFactory.newLatLngZoom(new LatLng(37.872508, -122.260783), 14.5f);
-        map.moveCamera(update);
-        busCallback = new BusCallback(map, mRefreshWrapper, timer, getBaseContext());
+        googleMap.moveCamera(update);
+        busCallback = new BusCallback(googleMap, mRefreshWrapper, timer, getBaseContext());
         liveTrack();
     }
 
@@ -91,13 +99,18 @@ public class LiveBusActivity extends AppCompatActivity implements OnMapReadyCall
         private ArrayList<Bus> buses;
         private Timer timer;
         private Context context;
+        private BitmapDescriptor icon;
 
         public BusCallback(GoogleMap map, LinearLayout refreshWrapper, Timer timer, Context context) {
             this.map = map;
             this.refreshWrapper = refreshWrapper;
             this.timer = timer;
             this.context = context;
+
+            Bitmap b = BitmapFactory.decodeResource(context.getResources(), R.drawable.transit_blue);
+            icon = BitmapDescriptorFactory.fromBitmap(Bitmap.createScaledBitmap(b, 75, 75, false));
         }
+
         @Override
         public synchronized void onDataRetrieved(Object data) {
             refreshWrapper.setVisibility(View.GONE);
@@ -107,7 +120,10 @@ public class LiveBusActivity extends AppCompatActivity implements OnMapReadyCall
             buses = (ArrayList<Bus>) data;
             map.clear();
             for(Bus bus : buses) {
-                map.addMarker(new MarkerOptions().position(bus.getLocation()));
+                map.addMarker(new MarkerOptions()
+                        .position(bus.getLocation())
+                        .icon(icon)
+                );
             }
         }
 
