bd917c7b42815ba36aa8a434379237179b0538a3
Jason Gao <jasonjlgao@gmail.com>
2016-10-10 00:03:50 -0700

    Initial work on resources

---
+++ app/src/main/java/com/asuc/asucmobile/controllers/ResourceController.java
@@ -0,0 +1,144 @@
+package com.asuc.asucmobile.controllers;
+
+import android.app.Activity;
+import android.content.Context;
+
+import com.asuc.asucmobile.models.Resource;
+import com.asuc.asucmobile.utilities.Callback;
+import com.asuc.asucmobile.utilities.CustomComparators;
+import com.asuc.asucmobile.utilities.JSONUtilities;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class ResourceController implements Controller {
+
+    private static final String URL = BASE_URL + "/resources";
+    private static final SimpleDateFormat DATE_FORMAT =
+            new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.ENGLISH);
+    private static final TimeZone PST = TimeZone.getTimeZone("America/Los_Angeles");
+
+    private static ResourceController instance;
+
+    private Context context;
+    private ArrayList<Resource> resources;
+    private Callback callback;
+
+    private Resource currentResource;
+
+    public static Controller getInstance(Context context) {
+        if (instance == null) {
+            instance = new ResourceController();
+        }
+
+        instance.context = context;
+
+        return instance;
+    }
+
+    public ResourceController() {
+        resources = new ArrayList<>();
+    }
+
+    @Override
+    public void setResources(final JSONArray array) {
+        if (array == null) {
+            ((Activity) context).runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    callback.onRetrievalFailed();
+                }
+            });
+            return;
+        }
+
+        resources.clear();
+
+        /*
+         *  Parsing JSON data into models is put into a background thread so that the UI thread
+         *  won't lag.
+         */
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (int i = 0; i < array.length(); i++) {
+                        JSONObject resourceJSON = array.getJSONObject(i);
+
+                        String resource = resourceJSON.getString("Resource");
+                        String topic = resourceJSON.getString("Topic");
+                        String phone1 = resourceJSON.getString("Phone 1");
+                        String phone2 = resourceJSON.getString("Phone 2 (Optional)");
+                        String email = resourceJSON.getString("Email");
+                        String location = resourceJSON.getString("Office Location");
+                        String hours = resourceJSON.getString("Hours");
+                        String onOrOffCampus = resourceJSON.getString("On/Off Campus");
+
+                        double lat;
+                        double lng;
+                        if (resourceJSON.getString("Latitude") != "null" && resourceJSON.getString("Longitude") != "null") {
+                            lat = resourceJSON.getDouble("Latitude");
+                            lng = resourceJSON.getDouble("Longitude");
+                        } else {
+                            /*
+                            Replace with the lat/long of Sproul Plaza, I guess.
+                            Right now this lat/long is whatever Bing says UC Berkeley is at.
+                            Notably this code allows resources to open but some resources have
+                                null lat/longs (this is carried over from libraries).
+                             */
+                            lat = 37.87;
+                            lng = -122.259;
+                        }
+
+                        String notes = resourceJSON.getString("Notes");
+
+                        resources.add(
+                                new Resource(resource, topic, phone1, phone2, email, location, hours, onOrOffCampus, lat, lng, notes));
+                    }
+
+                    // Sort the resources alphabetically, putting favorites at top
+                    Collections.sort(resources, CustomComparators.FacilityComparators.getSortByFavoriteResource(context));
+
+                    ((Activity) context).runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            callback.onDataRetrieved(resources);
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    ((Activity) context).runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            callback.onRetrievalFailed();
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    public void refreshInBackground(Callback callback) {
+        this.callback = callback;
+        JSONUtilities.readJSONFromUrl(URL, "resources", ResourceController.getInstance(context));
+    }
+
+    public void setCurrentResource(Resource resource) {
+        currentResource = resource;
+    }
+
+    public Resource getCurrentResource() {
+        return currentResource;
+    }
+
+}
+
---
+++ app/src/main/java/com/asuc/asucmobile/fragments/ResourceFragment.java
@@ -0,0 +1,201 @@
+package com.asuc.asucmobile.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.Filter;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.asuc.asucmobile.R;
+import com.asuc.asucmobile.adapters.ResourceAdapter;
+import com.asuc.asucmobile.controllers.ResourceController;
+import com.asuc.asucmobile.main.ListOfFavorites;
+import com.asuc.asucmobile.main.OpenResourceActivity;
+import com.asuc.asucmobile.models.Resource;
+import com.asuc.asucmobile.utilities.Callback;
+import com.asuc.asucmobile.utilities.CustomComparators;
+import com.asuc.asucmobile.utilities.NavigationGenerator;
+import com.asuc.asucmobile.utilities.SerializableUtilities;
+import com.flurry.android.FlurryAgent;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResourceFragment extends Fragment {
+
+    private ListView mResourceList;
+    private ProgressBar mProgressBar;
+    private LinearLayout mRefreshWrapper;
+
+    private ResourceAdapter mAdapter;
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        FlurryAgent.onStartSession(getContext(), "4VPTT49FCCKH7Z2NVQ26");
+        View layout = inflater.inflate(R.layout.fragment_resource, container, false);
+
+        Toolbar toolbar = (Toolbar) layout.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        NavigationGenerator.generateToolbarMenuButton(toolbar);
+        setHasOptionsMenu(true);
+        toolbar.setTitle("Resources");
+
+        ListOfFavorites listOfFavorites = (ListOfFavorites) SerializableUtilities.loadSerializedObject(getContext());
+        if (listOfFavorites == null) {
+            listOfFavorites = new ListOfFavorites();
+            SerializableUtilities.saveObject(getContext(), listOfFavorites);
+        }
+
+        ImageButton refreshButton = (ImageButton) layout.findViewById(R.id.refresh_button);
+
+        mResourceList = (ListView) layout.findViewById(R.id.resource_list);
+        mProgressBar = (ProgressBar) layout.findViewById(R.id.progress_bar);
+        mRefreshWrapper = (LinearLayout) layout.findViewById(R.id.refresh);
+
+        mAdapter = new ResourceAdapter(getContext());
+        mResourceList.setAdapter(mAdapter);
+
+        mResourceList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                ResourceController controller = ((ResourceController) ResourceController.getInstance(getContext()));
+                controller.setCurrentResource(mAdapter.getItem(i));
+                Intent intent = new Intent(getContext(), OpenResourceActivity.class);
+
+                //Flurry log for tapping Resource hours.
+                Map<String, String> libParams = new HashMap<>();
+                libParams.put("Hall", mAdapter.getItem(i).getResource());
+                FlurryAgent.logEvent("Taps Resource Hours", libParams);
+
+                startActivity(intent);
+            }
+        });
+
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                refresh();
+            }
+        });
+        refresh();
+
+        return layout;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        NavigationGenerator.closeMenu();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        FlurryAgent.onEndSession(getContext());
+    }
+
+    //start off lv sorted by favorites
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+
+        switch (menuItem.getItemId()){
+            case R.id.sortAZ:
+                Collections.sort(mAdapter.getResources(), CustomComparators.FacilityComparators.getSortResourcesByAZ());
+                mAdapter.notifyDataSetChanged();
+                break;
+            case R.id.sortFavorites:
+                Collections.sort(mAdapter.getResources(), CustomComparators.FacilityComparators.getSortByFavoriteResource(getContext()));
+                mAdapter.notifyDataSetChanged();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.resource, menu);
+
+        final MenuItem searchMenuItem = menu.findItem(R.id.search);
+
+        if (searchMenuItem != null) {
+            final SearchView searchView = (SearchView) searchMenuItem.getActionView();
+            if (searchView != null) {
+                // Setting up aesthetics
+                EditText searchEditText = (EditText) searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+                searchEditText.setTextColor(getResources().getColor(android.R.color.white));
+                searchEditText.setHintTextColor(getResources().getColor(android.R.color.white));
+
+                searchView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //Flurry log for searching for something!
+                        FlurryAgent.logEvent("Tapped on the Search Button (Resources)");
+                    }
+                });
+                searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                    @Override
+                    public boolean onQueryTextSubmit(String s) {
+                        // Close the keyboard
+                        searchView.clearFocus();
+                        return true;
+                    }
+
+                    @Override
+                    public boolean onQueryTextChange(String s) {
+                        final Filter filter = mAdapter.getFilter();
+                        filter.filter(s);
+                        return true;
+                    }
+                });
+            }
+        }
+    }
+
+    /**
+     * refresh() updates the visibility of necessary UI elements and refreshes the resource list
+     * from the web.
+     */
+    private void refresh() {
+        mResourceList.setVisibility(View.GONE);
+        mRefreshWrapper.setVisibility(View.GONE);
+        mProgressBar.setVisibility(View.VISIBLE);
+
+        ResourceController.getInstance(getContext()).refreshInBackground(new Callback() {
+            @Override
+            @SuppressWarnings("unchecked")
+            public void onDataRetrieved(Object data) {
+                mResourceList.setVisibility(View.VISIBLE);
+                mProgressBar.setVisibility(View.GONE);
+
+                mAdapter.setList((ArrayList<Resource>) data);
+            }
+
+            @Override
+            public void onRetrievalFailed() {
+                mProgressBar.setVisibility(View.GONE);
+                mRefreshWrapper.setVisibility(View.VISIBLE);
+                Toast.makeText(getContext(), "Unable to retrieve data, please try again", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
