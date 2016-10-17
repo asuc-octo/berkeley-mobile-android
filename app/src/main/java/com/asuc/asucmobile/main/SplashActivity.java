482a6b4803b73003484e0d07d4ac5f953ad89dae
Alex Kang <akang95@gmail.com>
2016-10-17 16:51:41 -0700

    flurry purge

--- app/src/main/java/com/asuc/asucmobile/main/SplashActivity.java
+++ app/src/main/java/com/asuc/asucmobile/main/SplashActivity.java
@@ -18,7 +18,8 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE); // Removes title bar
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);// Removes notification bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); // Removes notification bar
         setContentView(R.layout.activity_splash);
 
         // Start timer and launch main activity
