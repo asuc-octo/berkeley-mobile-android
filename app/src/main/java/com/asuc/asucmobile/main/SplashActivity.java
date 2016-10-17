b04b7ced23a09dbba22fe4da045c0a3967f4649b
Alex Kang <akang95@gmail.com>
2016-10-17 13:29:37 -0700

    Adjusted layout heights

--- app/src/main/java/com/asuc/asucmobile/main/SplashActivity.java
+++ app/src/main/java/com/asuc/asucmobile/main/SplashActivity.java
@@ -17,10 +17,8 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         requestWindowFeature(Window.FEATURE_NO_TITLE); // Removes title bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);// Removes notification bar
-
         setContentView(R.layout.activity_splash);
 
         // Start timer and launch main activity
