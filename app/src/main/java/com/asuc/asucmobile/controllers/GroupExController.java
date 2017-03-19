c26931714a259ea35811e4a7fc8eb363e890094e
Ankit Patel <ankitpatel96@gmail.com>
2017-03-18 20:03:01 -0700

    groupex global search fix

--- app/src/main/java/com/asuc/asucmobile/controllers/GroupExController.java
+++ app/src/main/java/com/asuc/asucmobile/controllers/GroupExController.java
@@ -6,12 +6,16 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public class GroupExController {
     public interface cService {
 
         @GET("group_exs/")
         Call<GroupExs> getData();
+
+        @GET("group_exs/{id}")
+        Call<GroupExs> getDatum(@Path("id") int id);
 
     }
 
