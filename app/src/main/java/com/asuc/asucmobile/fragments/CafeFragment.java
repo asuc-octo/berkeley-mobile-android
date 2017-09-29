d470db1a5266e2b244edce351a1e14fb0053bd39
rustielin <rustielin@berkeley.edu>
2017-09-29 01:19:54 -0700

    made a bunch of cafe files

---
+++ app/src/main/java/com/asuc/asucmobile/fragments/CafeFragment.java
@@ -0,0 +1,34 @@
+package com.asuc.asucmobile.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by rustie on 9/29/17.
+ */
+
+public class CafeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    /**
+     * refresh() updates the visibility of necessary UI elements and refreshes the cafe list
+     * from the web.
+     */
+    private void refresh() {
+
+    }
+}
---
+++ app/src/main/java/com/asuc/asucmobile/main/OpenCafeActivity.java
@@ -0,0 +1,8 @@
+package com.asuc.asucmobile.main;
+
+/**
+ * Created by rustie on 9/28/17.
+ */
+
+public class OpenCafeActivity extends BaseActivity {
+}
