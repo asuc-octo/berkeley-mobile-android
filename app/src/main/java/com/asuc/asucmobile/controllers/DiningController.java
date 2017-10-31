9378357fbb757baf8127ee8077fc810965929f9f
sudarshanGopal98 <32005488+sudarshanGopal98@users.noreply.github.com>
2017-10-30 17:15:32 -0700

    fixes on limited and some cafe stuff
    updated cafe and dining controllers, made some changes to FoodItem class and cafe's xml file

--- app/src/main/java/com/asuc/asucmobile/controllers/DiningController.java
+++ app/src/main/java/com/asuc/asucmobile/controllers/DiningController.java
@@ -78,34 +78,34 @@ public class DiningController implements Controller {
                         ArrayList<FoodItem> breakfastMenu = new ArrayList<>();
                         for (int j = 0; j < breakfastJSON.length(); j++) {
                             JSONObject foodJSON = breakfastJSON.getJSONObject(j);
-                            // get the food types
+                            /*// get the food types
                             ArrayList<String> foodTypes = new ArrayList<>();
                             if (foodJSON.has("food_type")) {
                                 JSONArray foodTypesArray = foodJSON.getJSONArray("food_type");
                                 for (int k = 0; k < foodTypesArray.length(); k++) {
                                     foodTypes.add(foodTypesArray.getString(k).toUpperCase());
                                 }
-                            }
+                            }*/
                             breakfastMenu.add(new FoodItem(
                                     foodJSON.getString("id"),
                                     foodJSON.getString("name"),
                                     foodJSON.getString("calories"),
                                     foodJSON.optDouble("cost"),
-                                    foodJSON.getString("food_type")
+                                    foodJSON.getString("food_type")));
                         }
                         Collections.sort(breakfastMenu, CustomComparators.FacilityComparators.getFoodSortByFavorite(context));
                         JSONArray lunchJSON = diningHall.getJSONArray("lunch_menu");
                         ArrayList<FoodItem> lunchMenu = new ArrayList<>();
                         for (int j = 0; j < lunchJSON.length(); j++) {
                             JSONObject foodJSON = lunchJSON.getJSONObject(j);
-                            // get the food types
+                            /*// get the food types
                             ArrayList<String> foodTypes = new ArrayList<>();
                             if (foodJSON.has("food_type")) {
                                 JSONArray foodTypesArray = foodJSON.getJSONArray("food_type");
                                 for (int k = 0; k < foodTypesArray.length(); k++) {
                                     foodTypes.add(foodTypesArray.getString(k).toUpperCase());
                                 }
-                            }
+                            }*/
                             lunchMenu.add(new FoodItem(
                                     foodJSON.getString("id"),
                                     foodJSON.getString("name"),
@@ -119,14 +119,14 @@ public class DiningController implements Controller {
                         ArrayList<FoodItem> dinnerMenu = new ArrayList<>();
                         for (int j = 0; j < dinnerJSON.length(); j++) {
                             JSONObject foodJSON = dinnerJSON.getJSONObject(j);
-                            // get the food types
+                            /*// get the food types
                             ArrayList<String> foodTypes = new ArrayList<>();
                             if (foodJSON.has("food_type")) {
                                 JSONArray foodTypesArray = foodJSON.getJSONArray("food_type");
                                 for (int k = 0; k < foodTypesArray.length(); k++) {
                                     foodTypes.add(foodTypesArray.getString(k).toUpperCase());
                                 }
-                            }
+                            }*/
                             dinnerMenu.add(new FoodItem(
                                     foodJSON.getString("id"),
                                     foodJSON.getString("name"),
