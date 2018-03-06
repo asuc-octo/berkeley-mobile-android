04f3f65e6f366bf68f628203de62d9ee4772159f
rustielin <rustielin@berkeley.edu>
2018-03-06 02:23:28 -0800

    gym class models

--- app/src/main/java/com/asuc/asucmobile/models/GymClass.java
+++ app/src/main/java/com/asuc/asucmobile/models/GymClass.java
@@ -1,142 +1,81 @@
 package com.asuc.asucmobile.models;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.sql.Time;
 import java.util.Date;
 
-/**
- * Created by sudarshan on 3/1/18.
- */
-
 public class GymClass {
-    public static final int ALL_AROUND = 1;
-    public static final int CARDIO = 2;
-    public static final int MIND = 3;
-    public static final int CORE = 4;
-    public static final int DANCE = 5;
-    public static final int STRENGTH = 6;
-    public static final int AQUA = 7;
-
 
     private int id;
-    private Date date;
-    private Date startTime;
-    private Date endTime;
+    private String date;
     private String name;
     private String description;
     private String trainer;
-    private int classType;
     private String location;
 
+    @SerializedName("image_link")
+    private String imageUrl;
 
-    public GymClass(int id, Date date, Date startTime, Date endTime, String name, String description, String trainer, int classType, String location) {
-        this.id = id;
-        this.date = date;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.name = name;
-        this.description = description;
-        this.trainer = trainer;
-        this.classType = classType;
-        this.location = location;
-    }
+    @SerializedName("class_type")
+    private String classType;
 
-    public static int getAllAround() {
-        return ALL_AROUND;
-    }
+    @SerializedName("start_time")
+    private Date start;
 
-    public static int getCARDIO() {
-        return CARDIO;
-    }
+    @SerializedName("end_time")
+    private Date end;
 
-    public static int getMIND() {
-        return MIND;
-    }
+    /**
+     * Whether this class is happening currently
+     * @return
+     */
+    public boolean isHappening() {
+        if (start == null || end == null) {
+            return false;
+        }
 
-    public static int getCORE() {
-        return CORE;
-    }
-
-    public static int getDANCE() {
-        return DANCE;
-    }
-
-    public static int getSTRENGTH() {
-        return STRENGTH;
-    }
-
-    public static int getAQUA() {
-        return AQUA;
+        Date currentTime = new Date();
+        return currentTime.after(start) && currentTime.before(end);
     }
 
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Date getDate() {
+    public String getDate() {
         return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public Date getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(Date startTime) {
-        this.startTime = startTime;
-    }
-
-    public Date getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(Date endTime) {
-        this.endTime = endTime;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public String getTrainer() {
         return trainer;
     }
 
-    public void setTrainer(String trainer) {
-        this.trainer = trainer;
-    }
-
-    public int getClassType() {
-        return classType;
-    }
-
-    public void setClassType(int classType) {
-        this.classType = classType;
-    }
-
     public String getLocation() {
         return location;
     }
 
-    public void setLocation(String location) {
-        this.location = location;
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getClassType() {
+        return classType;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public Date getEnd() {
+        return end;
     }
 }
