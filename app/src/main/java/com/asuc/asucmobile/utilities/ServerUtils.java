d61672a19c538250f305352d35dace108c3594cd
rustielin <rustielin@berkeley.edu>
2018-04-28 01:42:46 -0700

    bfg

---
+++ app/src/main/java/com/asuc/asucmobile/utilities/ServerUtils.java
@@ -0,0 +1,14 @@
+package com.asuc.asucmobile.utilities;
+
+/**
+ * Created by rustie on 3/26/18.
+ */
+
+public class ServerUtils {
+
+    private static final String BASE_URL = "https://berkeleymobile-sandbox.herokuapp.com/api/";
+
+    public static String getBaseUrl() {
+        return BASE_URL;
+    }
+}
