f2cd363aa7584cd7b6ea56ae08d87eb5cc174a99
ylkao <yiling.data@gmail.com>
2018-10-20 17:55:27 -0700

    Fixed bounds for Destination

--- app/src/main/java/com/asuc/asucmobile/fragments/DestinationFragment.java
+++ app/src/main/java/com/asuc/asucmobile/fragments/DestinationFragment.java
@@ -70,8 +70,8 @@ public class DestinationFragment extends PlaceAutocompleteFragment implements
 
     private GoogleApiClient mGoogleApiClient;
     private PlaceArrayAdapter mPlaceArrayAdapter;
-    private static final LatLngBounds BOUNDS_MOUNTAIN_VIEW = new LatLngBounds(
-            new LatLng(37.851532, -122.232216), new LatLng(37.902479, -122.313240));
+    private static final LatLngBounds BOUNDS_BERKELEY = new LatLngBounds(
+            new LatLng(37.854866, -122.269154), new LatLng(37.883038, -122.245781));
     private LatLng destination;
 
 
@@ -137,7 +137,7 @@ public class DestinationFragment extends PlaceAutocompleteFragment implements
 
 
         mPlaceArrayAdapter = new PlaceArrayAdapter(getContext(), R.layout.maps_autocomplete_list_item, R.id.text1,
-                BOUNDS_MOUNTAIN_VIEW, filter);
+                BOUNDS_BERKELEY, filter);
         mAutocompleteTextView.setAdapter(mPlaceArrayAdapter);
 
         return layout;
