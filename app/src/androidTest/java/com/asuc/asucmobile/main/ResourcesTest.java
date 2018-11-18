8603f827e425d5b38fe4022c5db20d43581307a0
Courtney Brousseau <courtney.brousseau@berkeley.edu>
2018-11-18 13:43:21 -0800

    Fixed timing for UI test

--- app/src/androidTest/java/com/asuc/asucmobile/main/ResourcesTest.java
+++ app/src/androidTest/java/com/asuc/asucmobile/main/ResourcesTest.java
@@ -60,7 +60,7 @@ public class ResourcesTest {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(60000);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
