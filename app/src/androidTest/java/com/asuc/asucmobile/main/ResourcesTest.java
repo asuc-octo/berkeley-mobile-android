3315c266862873def64a180e680d52eaabfb961a
Courtney Brousseau <courtney.brousseau@berkeley.edu>
2018-11-18 13:28:14 -0800

    Added longer UI test

--- app/src/androidTest/java/com/asuc/asucmobile/main/ResourcesTest.java
+++ app/src/androidTest/java/com/asuc/asucmobile/main/ResourcesTest.java
@@ -1,6 +1,7 @@
 package com.asuc.asucmobile.main;
 
 
+import android.support.test.espresso.DataInteraction;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -14,45 +15,65 @@ import com.asuc.asucmobile.R;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class SplashActivityTest {
+public class ResourcesTest {
 
     @Rule
     public ActivityTestRule<SplashActivity> mActivityTestRule = new ActivityTestRule<>(SplashActivity.class);
 
     @Test
-    public void splashActivityTest() {
+    public void resourcesTest() {
         // Added a sleep statement to match the app's execution delay.
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(1000);
+            Thread.sleep(3595766);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
+        DataInteraction linearLayout = onData(anything())
+                .inAdapterView(allOf(withId(R.id.drawer_list),
+                        childAtPosition(
+                                withId(R.id.left_drawer),
+                                1)))
+                .atPosition(4);
+        linearLayout.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(60000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         ViewInteraction textView = onView(
-                allOf(withId(R.id.menu_name), withText("BearTransit"),
+                allOf(withId(R.id.resource), withText("Cal Student Central"),
                         childAtPosition(
                                 childAtPosition(
-                                        withId(R.id.drawer_list),
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
                                         0),
-                                1),
+                                0),
                         isDisplayed()));
-        textView.check(matches(withText("BearTransit")));
+        textView.check(matches(withText("Cal Student Central")));
 
     }
 
