6b73afe2a608a357717774a0404977e5fa1f3df9
Derrick Li <derrickli@berkeley.edu>
2019-01-17 18:10:52 -0800

    updated first session definition and added comments

---
+++ app/src/main/java/com/asuc/asucmobile/utilities/RoundedBackgroundSpan.java
@@ -0,0 +1,37 @@
+package com.asuc.asucmobile.utilities;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.text.style.LineBackgroundSpan;
+import android.text.style.ReplacementSpan;
+
+public class RoundedBackgroundSpan implements LineBackgroundSpan
+{
+    private int mBackgroundColor;
+    private int mPadding;
+    private Rect mBgRect;
+
+    public RoundedBackgroundSpan(int backgroundColor, int padding) {
+        super();
+        mBackgroundColor = backgroundColor;
+        mPadding = padding;
+        // Precreate rect for performance
+        mBgRect = new Rect();
+    }
+
+    @Override
+    public void drawBackground(Canvas c, Paint p, int left, int right, int top, int baseline, int bottom, CharSequence text, int start, int end, int lnum) {
+        final int textWidth = Math.round(p.measureText(text, start, end));
+        final int paintColor = p.getColor();
+        // Draw the background
+        mBgRect.set(left - mPadding,
+                top - (lnum == 0 ? mPadding / 2 : - (mPadding / 2)),
+                left + textWidth + mPadding,
+                bottom + mPadding / 2);
+        p.setColor(mBackgroundColor);
+        c.drawRect(mBgRect, p);
+        p.setColor(paintColor);
+    }
+}
